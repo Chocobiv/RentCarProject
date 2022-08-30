f8de94a1cbb5c5ef94ff7b95c1278fab5febb016
Chocobiv <47735101+Chocobiv@users.noreply.github.com>
2022-08-30 09:49:51 +0900

    Add files via upload
    upload

---
+++ src/rentCar/controller/CarController.java
@@ -0,0 +1,14 @@
+package rentCar.controller;
+
+import rentCar.model.Dao.Dao;
+
+public class CarController {
+
+    //1. 등록 서비스
+    public void register(){
+        Dao.getDao().register();
+    }
+    //2. 삭제 서비스
+    //3. 수정 서비스 등등등
+
+}
