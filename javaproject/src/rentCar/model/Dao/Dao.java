017c3321baf0844dea29908033c42106233cb7a9
Chocobiv <47735101+Chocobiv@users.noreply.github.com>
2022-08-30 09:29:09 +0900

    Add files via upload
    upload

---
+++ javaproject/src/rentCar/model/Dao/Dao.java
@@ -0,0 +1,36 @@
+package rentCar.model.Dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class Dao {
+    protected Connection con;
+    protected ResultSet rs;
+    protected PreparedStatement ps;
+
+    // 2. 생성자
+    public Dao() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/rent_car2?serverTimezone=UTC" , "root","1234");
+        }
+        catch (Exception e) {System.out.println("[연동 실패]");}
+    }
+    public static Dao dao = new Dao() ; 	// 3. Dao 객체 생성
+    public static Dao getDao() { return dao; } // 4. Dao 객체 반환
+
+    //1. 차량 등록 테스트 메소드
+    public boolean register( ) {
+        String sql = "INSERT INTO `차량` VALUES ('00누 5555','k7','준대형','그레이','LPG','에어백',5,'부산','-',80000)";
+        try {
+            ps = con.prepareStatement(sql);
+            ps.executeUpdate();
+            return true;
+        }
+        catch (Exception e) {System.out.println(e);}
+        return false;
+    }
+
+}
---
+++ javaproject/src/rentCar/model/Dto/CarDto.java
@@ -0,0 +1,7 @@
+package rentCar.model.Dto;
+
+public class CarDto {
+    //1.필드
+    //2.생성자
+    //3.메소드
+}
