33308b0c640acda72588de96d6deb5b341c9240f
Chocobiv <chocobiv123@gmail.com>
2022-08-31 19:30:49 +0900

    [22/08/31 19:30] 주석 추가

--- src/rentCar/controller/CarController.java
+++ src/rentCar/controller/CarController.java
@@ -3,7 +3,7 @@ package rentCar.controller;
 import rentCar.model.Dao.MemberDao;
 
 /* 유효성 검사 후 main으로 결과 반환해서 그 다음에 Dto로 값 담아서 Dao로! 그리고 Dao에서 DB로 */
-
+/* equls() 또는 정규표현식 사용 */
 public class CarController {
 
     //1. 등록 서비스 (고객 회원가입)
