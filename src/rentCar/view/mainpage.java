cf43ca686d2728ce20aaf1d71d29e238dcfded35
Chocobiv <chocobiv123@gmail.com>
2022-09-01 00:33:31 +0900

    [22/09/01 00:30] 개행문자 제거 -> 운전면허증번호 입력 안되는 이슈 해결

--- src/rentCar/view/mainpage.java
+++ src/rentCar/view/mainpage.java
@@ -42,6 +42,9 @@ public class mainpage {
                         System.out.println("| 회원가입 |");
                         System.out.println(" ---------");
 
+                        //개행문자 제거 - 운전면허증번호 입력을 안받고 넘어가는 문제 발생 -> 해결ㄴ
+                        scanner.nextLine();
+
                         //입력하라는 말 써야함. 그리고 형식 예시도 필요
                         //비밀번호 확인도 해야함 -> controller에서 (근데 DB에 비밀번호가 없음!)
                         System.out.print("운전면허증번호를 입력하세요 (예시.12-12-123456-12): ");
@@ -64,10 +67,10 @@ public class mainpage {
                         MemberDto member = new MemberDto(inputDriveNum,inputDriveDate,inputBirth,inputName,inputAge,inputAddr,inputPhoneNum,inputEmail);
                         System.out.println( member.toString() );
 
-                        /* 유효성 검사 - Controller에서 */
+                        //* 유효성 검사 - Controller에서 *//*
                         carController.signup();
 
-                        /* 입력받은 값 Dto에 담아서 Dao로 */
+                        //* 입력받은 값 Dto에 담아서 Dao로 *//*
                         boolean result = MemberDao.getmemberDao().signup( member );
                         if( result ) {
                             System.out.println("성공");
